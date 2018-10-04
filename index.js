6a594d79395f85bd4fa81e3add86f9bf36ea82f8
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-04 16:48:15 +0000

    Done.

--- index.js
+++ index.js
@@ -18,30 +18,19 @@ function increaseRankBy(n) {
     queue.push(...Array.from(currentList.querySelectorAll('ul')))
     items.push(...Array.from(currentList.querySelectorAll('li')))
   }
-
   for (let item of items) {
     let rank = parseInt(item.innerHTML)
     item.innerHTML = (rank+n).toString()
   }
-  console.log(items.map(x => x.innerHTML));
-  // increases the ranks in all of the .ranked-lists by n. (You might need to make use of parseInt()
 }
 
 function deepestChild() {
-  // pulls out the most deeply nested child from div#grand-node.
-  // (Remember, you can iterate over elements and call querySelector() and querySelectorAll() on them.
-  var node = document.querySelector('#grand-node')
-
-  let queue = [node]
-  let list = [node]
+  let queue = [document.querySelector('#grand-node')]
   let current
   while (current = queue.shift()) {
-    if (current.children.length) {
-      queue.push(...current.children)
-      list.push(...current.children)
-    } else {
+    if (!current.children.length) {
       return current
     }
+    queue.push(...current.children)
   }
-  // Your solution for deepestChild() does not need to be totally generic; we don't expect it to work in every case. For example, we know that div#grand-node has only one node at each level — for this lab, you can solve for that case, and not worry about a case where there are sibling nodes.
 }
